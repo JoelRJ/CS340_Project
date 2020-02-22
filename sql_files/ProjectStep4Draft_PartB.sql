a71eaa858e9c80c3b3c011d64690f8e12bef236a
Joel Johnson <johnsjoe@flip3.engr.oregonstate.edu>
2020-02-22 15:44:23 -0800

    SQL updates to part B

--- sql_files/ProjectStep4Draft_PartB.sql
+++ sql_files/ProjectStep4Draft_PartB.sql
@@ -2,28 +2,42 @@
 -- Date: 2/22/2020
 -- Website: http://web.engr.oregonstate.edu/~renteril/CS340_Project/index.html
 
-
+-- ---------------------------
 -- Trainer Pokemon Index Table
-SELECT trainers.name, pokemon.name FROM trainers_pokemon
-    JOIN trainers ON trainers.trainerID = trainers_pokemon.trainer,
-    JOIN pokemon ON pokemon.pokemonID = trainers_pokemon.pokemonID;
 
+-- Fill table SELECT
+SELECT id, trainers.name AS "Trainer", pokemon.name AS "Pokemon" FROM trainers_pokemon
+	JOIN pokemon ON pokemon.pokemonID = trainers_pokemon.pokemonID
+    JOIN trainers ON trainers.trainerID = trainers_pokemon.trainerID
 
+-- Trainer drop-down menu
+SELECT name FROM trainers;
+
+-- Pokemon drop-down menu
+SELECT name FROM pokemon;
+
+-- Insert button
 INSERT INTO trainers_pokemon (trainer, pokemon)
-    VALUES (:trainerInput, :pokemonInput);
-
+    VALUES ((SELECT trainerID FROM trainers WHERE name = :trainerInput), (SELECT pokemonID FROM pokemon WHERE name = :pokemonInput);
+    
+-- Update button
 UPDATE trainers_pokemon 
     SET trainer = :trainerInput, Pokemon = :pokemonInput
     WHERE id = :rowID;
-    
+
+-- Delete button
 DELETE FROM trainers_pokemon WHERE id = :rowID;
 
-SELECT trainer
-
+-- ---------------------------
 -- Attacks Table
+SELECT * FROM attacks;
+
 INSERT INTO attacks (description)
     VALUES (:attackInput);
-    
+
+-- ---------------------------
 -- Defenses Table
+SELECT * FROM defenses;
+
 INSERT INTO defenses (description)
     VALUES (:defenseInput);
