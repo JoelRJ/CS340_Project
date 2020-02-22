64247499bbefe0b4a4bc54c4607e7f0ab827d418
renteril <49972056+renteril@users.noreply.github.com>
2020-02-22 11:09:17 -0800

    Delete battles.sql

--- battles.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO battles
-  (winner, loser) 
-VALUES 
-  ((SELECT trainerID FROM trainers WHERE name = 'Ash Ketchum'), (SELECT trainerID FROM trainers WHERE name = 'Hasty Bogey')),
-  ((SELECT trainerID FROM trainers WHERE name = 'Audre Harberer'), (SELECT trainerID FROM trainers WHERE name = 'Waring Casero')),
-  ((SELECT trainerID FROM trainers WHERE name = 'Corby Woolforde'), (SELECT trainerID FROM trainers WHERE name = 'Shandra Amdtsen'));
