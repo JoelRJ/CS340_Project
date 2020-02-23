912d1d25babc0cfc06501220134c19051b271f89
Luis <luisrent84@gmail.com>
2020-02-22 21:21:34 -0800

    added queries

--- sql_files/ProjectStep4Draft_PartB.sql
+++ sql_files/ProjectStep4Draft_PartB.sql
@@ -41,3 +41,91 @@ SELECT * FROM defenses;
 
 INSERT INTO defenses (description)
     VALUES (:defenseInput);
+
+
+-----------------------------------------
+
+-- POKEMON TABLE
+
+-- View Pokemon Table (SELECT)
+SELECT pokemon.name, pt1.description AS type1, pt2.description AS type2, attacks.description AS attack, 
+defenses.description AS defense, pokemon.height, pokemon.weight  
+FROM pokemon
+    JOIN pokemonTypes pt1 ON pt1.typeID = pokemon.type1
+    JOIN pokemonTypes pt2 ON pt2.typeID = pokemon.type2
+    JOIN attacks ON pokemon.attack = attacks.attackID
+    JOIN defenses ON pokemon.defense = defenses.defenseID;
+
+-- Add Trainer (INSERT)
+INSERT INTO pokemon
+  (name, type1, type2, attack, defense, weight, height) 
+VALUES 
+  (:nameInput, 
+    (SELECT typeID FROM pokemonTypes WHERE description = :type1Input), 
+    (SELECT typeID FROM pokemonTypes WHERE description = :type2Input),
+    (SELECT attackID FROM attacks WHERE description = :attackInput),
+    (SELECT defenseID FROM defenses WHERE description = :defenseInput),
+    :weightInput,
+    :heightInput);
+
+-- Search Pokemon (Drop Down - SELECT)
+SELECT FROM pokemon WHERE id = :rowID;
+
+-- Make 'attack' Attribute NULL (UPDATE)
+UPDATE pokemon
+    SET attack = NULL
+    WHERE pokemonID = :rowID;
+
+-----------------------------------------
+
+-- TRAINER TABLE
+
+-- View Trainer Table (SELECT)
+SELECT * FROM trainers;
+
+-- Add Trainer (INSERT)
+INSERT INTO  trainers
+  (name, gender, age) 
+  VALUES 
+  (:nameInput, :genderInput, :ageInput);
+
+-- Remove Trainer (DELETE)
+DELETE FROM trainers WHERE trainerID = :rowID;
+
+-----------------------------------------
+
+-- BATTLE TABLE
+
+-- View Battle Table (SELECT)
+SELECT battles.battleID, t1.name AS winner, t2.name AS loser
+FROM battles
+    JOIN trainers t1 ON battles.winner = t1.trainerID
+	JOIN trainers t2 ON battles.loser = t2.trainerID;
+
+
+-- Add Winner and Loser (INSERT)
+INSERT INTO battles
+  (winner, loser) 
+VALUES 
+  ((SELECT trainerID FROM trainers WHERE name = :winnerInput), 
+  	(SELECT trainerID FROM trainers WHERE name = :loserInput));
+
+-- Update Winner and Loser (UPDATE)
+
+UPDATE 
+    SET winner = (SELECT trainerID FROM trainers WHERE name = :winnerInput),
+        loser = (SELECT trainerID FROM trainers WHERE name = :loserInput)
+    WHERE battleID = :rowID;
+
+
+-----------------------------------------
+-- POKEMON TYPES TABLE
+
+-- View Pokemon Types Table (SELECT)
+SELECT * FROM pokemonTypes;
+
+-- Add Pokemon Types (INSERT)
+INSERT INTO  pokemonTypes
+  (description) 
+VALUES 
+  (:typeInput)
